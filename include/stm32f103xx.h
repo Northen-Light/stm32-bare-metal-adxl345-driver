bc0076cbad7cef41796f2e3eb9f0fc2aa013a95e
Prateek Patnaik <prateekpatnaikcomp@gmail.com>
2026-09-11 20:33:34 +0530

    Add bit, bit-field, bit-mask macros for I2C peripheral driver

--- include/stm32f103xx.h
+++ include/stm32f103xx.h
@@ -16,4 +16,30 @@
 #define I2C_CCR                               *((volatile uint32_t *) 0x4000541CU)  
 #define I2C_TRISE                             *((volatile uint32_t *) 0x40005420U)  
 
+
+#define RCC_APB2ENR_IOPBEN                    (1U << 3)
+#define RCC_APB1ENR_I2C1EN                    (1U << 21)
+
+#define GPIOB_CRL_PIN6_MASK                   (0xFU << 24)
+#define GPIOB_CRL_PIN6_ALT_OD_2MHZ            (0xEU << 24)
+#define GPIOB_CRL_PIN7_MASK                   (0xFU << 28)
+#define GPIOB_CRL_PIN7_ALT_OD_2MHZ            (0xEU << 28)
+
+#define I2C1_CR1_RESET                        0
+#define I2C1_CR2_FREQ_MASK                    (0x3FU << 0)
+#define I2C1_CR2_FREQ_PCLK1_8MHZ              (0x8U << 0)
+#define I2C_CCR_MASK                          (0xFFFU << 0)
+#define I2C_CCR_SM_100KHZ                     (0x28U << 0)
+#define I2C1_TRISE_FREQ_MASK                  (0x3FU << 0)
+#define I2C1_TRISE_1000_NS                    (0x9U << 0)  
+#define I2C1_CR1_PE                           (1U << 0) 
+#define I2C1_CR1_START                        (1U << 8)    
+#define I2C1_CR1_STOP                         (1U << 9)   
+#define I2C1_CR1_ACK                          (1U << 10) 
+#define I2C1_SR1_SB                           (1U << 0)
+#define I2C1_SR1_ADDR                         (1U << 1)
+#define I2C1_SR1_BTF                          (1U << 2)
+#define I2C1_SR1_RXNE                         (1U << 6)
+#define I2C1_SR1_TXE                          (1U << 7)
+
 #endif
--- src/i2c.c
+++ src/i2c.c
@@ -16,23 +16,23 @@ static void i2c1_set_NACK(void);
 static void i2c1_clear_addr(void);
 
 void i2c1_init(void) {
-  RCC_APB2ENR |= (1U << 3);
-  RCC_APB1ENR |= (1U << 21);
+  RCC_APB2ENR |= RCC_APB2ENR_IOPBEN;
+  RCC_APB1ENR |= RCC_APB1ENR_I2C1EN;
 
-  GPIOB_CRL &= ~(0xFU << 24);
-  GPIOB_CRL |= (0xEU << 24);
-  GPIOB_CRL &= ~(0xFU << 28);
-  GPIOB_CRL |= (0xEU << 28);
+  GPIOB_CRL &= ~GPIOB_CRL_PIN6_MASK;
+  GPIOB_CRL |= GPIOB_CRL_PIN6_ALT_OD_2MHZ;
+  GPIOB_CRL &= ~GPIOB_CRL_PIN7_MASK;
+  GPIOB_CRL |= GPIOB_CRL_PIN7_ALT_OD_2MHZ;
 
-  I2C1_CR1 = 0;
-  I2C1_CR2 &= ~(0x3FU << 0);
-  I2C1_CR2 |= (0x8U << 0);
-  I2C_CCR &= ~(0xFFFU << 0);
-  I2C_CCR |= (0x28U << 0);
-  I2C_TRISE &= ~(0x3FU << 0);
-  I2C_TRISE |= (0x9U << 0);
+  I2C1_CR1 = I2C1_CR1_RESET;
+  I2C1_CR2 &= ~I2C1_CR2_FREQ_MASK;
+  I2C1_CR2 |= I2C1_CR2_FREQ_PCLK1_8MHZ;
+  I2C_CCR &= ~I2C_CCR_MASK;
+  I2C_CCR |= I2C_CCR_SM_100KHZ;
+  I2C_TRISE &= ~I2C1_TRISE_FREQ_MASK;
+  I2C_TRISE |= I2C1_TRISE_1000_NS;
 
-  I2C1_CR1 |= (1U << 0);
+  I2C1_CR1 |= I2C1_CR1_PE;
 }
 
 void i2c1_single_byte_slave_read(uint8_t slave_address, uint8_t register_address, uint8_t *byte) {
@@ -53,7 +53,7 @@ void i2c1_single_byte_slave_read(uint8_t slave_address, uint8_t register_address
 }
 
 static void i2c1_start(void) {
-  I2C1_CR1 |= (1U << 8);
+  I2C1_CR1 |= I2C1_CR1_START;
   i2c1_wait_for_SB();
 }
 
@@ -63,7 +63,7 @@ static void i2c1_send_address(uint8_t address, uint8_t direction) {
 }
 
 static void i2c1_stop(void) {
-  I2C1_CR1 |= (1U << 9);
+  I2C1_CR1 |= I2C1_CR1_STOP;
 }
 
 static void i2c1_write_byte(uint8_t byte) {
@@ -77,31 +77,31 @@ static void i2c1_read_byte(uint8_t *byte) {
 }
 
 static void i2c1_wait_for_SB(void) {
-  while ((I2C1_SR1 & (1U << 0)) == 0) {}
+  while ((I2C1_SR1 & I2C1_SR1_SB) == 0) {}
 }
 
 static void i2c1_wait_for_ADDR(void) {
-  while ((I2C1_SR1 & (1U << 1)) == 0) {}
+  while ((I2C1_SR1 & I2C1_SR1_ADDR) == 0) {}
 }
 
 static void i2c1_wait_for_BTF(void) {
-  while ((I2C1_SR1 & (1U << 2)) == 0) {}
+  while ((I2C1_SR1 & I2C1_SR1_BTF) == 0) {}
 }
 
 static void i2c1_wait_for_RXNE(void) {
-  while ((I2C1_SR1 & (1U << 6)) == 0) {}
+  while ((I2C1_SR1 & I2C1_SR1_RXNE) == 0) {}
 }
 
 static void i2c1_wait_for_TXE(void) {
-  while ((I2C1_SR1 & (1U << 7)) == 0) {}
+  while ((I2C1_SR1 & I2C1_SR1_TXE) == 0) {}
 }
 
 static void i2c1_set_ACK(void) {
-  I2C1_CR1 |= (1U << 10);
+  I2C1_CR1 |= I2C1_CR1_ACK;
 }
 
 static void i2c1_set_NACK(void) {
-  I2C1_CR1 &= ~(1U << 10);
+  I2C1_CR1 &= ~I2C1_CR1_ACK;
 }
 
 static void i2c1_clear_addr(void) {
